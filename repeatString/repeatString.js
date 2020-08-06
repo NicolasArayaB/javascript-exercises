c022341e53cfaee0a98cc4e786b4bb08a040491f
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-06 18:44:31 -0400

    done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let repeatedText = "";
+    if (num >= 0) {
+       for (let i = 0; i < num; i++) {
+        repeatedText += text;
+        }
+    return repeatedText; 
+    }
+    else {
+        return 'ERROR'
+    }
+    
 }
 
 module.exports = repeatString
