1b107edf0ce970ce43fb3afacbc890defd76de78
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-07 16:30:15 -0400

    multiple arguments

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let args = Array.from(arguments);
+    let array = args[0]
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === args[i]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
