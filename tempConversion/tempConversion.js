d694268a8d1971e54e5a2e58397edea46fd7fa5f
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-07 18:08:15 -0400

    done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5/9;
+  if (Number.isInteger(c)) {
+    return c;
+  }
+  else {
+    return Number(c.toFixed(1));
+  }
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * 9 / 5 + 32;
+  if (Number.isInteger(f)) {
+    return (f);
+  }
+  else {
+    return Number(f.toFixed(1));
+  }
 }
 
 module.exports = {
