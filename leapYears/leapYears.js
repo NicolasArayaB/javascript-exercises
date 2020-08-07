84274d3e6387dd89d87d91b65f721813b1ad28d8
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-07 17:27:32 -0400

    done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let nonCentury = year % 4;
+    let century = year % 100;
+    let leapCentury = year % 400;
 
+    if (century == 0 && leapCentury == 0) {
+        return true;
+    }
+    else if (nonCentury == 0 && century != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 module.exports = leapYears
