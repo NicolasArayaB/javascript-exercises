d728afae689db54b95d75a3f48afe091be9fc5f3
NicolasArayaB <nicolasaraya@gmail.com>
2020-08-07 17:02:18 -0400

    done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let numA = Number(a);
+    let numB = Number(b);
+    let sum = 0;
+    if (typeof(a) != "number" || typeof(b) != "number"){
+        return 'ERROR';
+    }    
+    if (numA < 0 || numB < 0) {
+        return 'ERROR';
+    }
+    if (numA < numB) {
+        for (let i = numA; i <= numB; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (numB < numA) {
+        for (let i = numB; i <= numA; i++) {
+            sum += i;
+        }
+        return sum;
+    }    
+    else {
+        sum = numA + numB;
+        return sum;
+    }
 }
 
 module.exports = sumAll
